fc057dc8b8be74161c4963d3fb55f24289c6906d
Sander Saares <saares@axinom.com>
2018-03-12 09:55:47 +0200

    Added tooling for building diagrams

---
+++ Tooling/Build-Diagrams.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+
+& (Join-Path $PSScriptRoot "Verify-Install.ps1")
+
+$inputPath = Join-Path (Split-Path -Parent $PSScriptRoot) "Diagrams"
+$outputPath = Join-Path (Split-Path -Parent $PSScriptRoot) "Diagrams-Png"
+
+# Remove any existing diagram build output.
+Get-ChildItem -Path $outputPath -Filter "*.png" | Remove-Item
+
+# And build the diagrams anew.
+$plantuml = Join-Path $PSScriptRoot "plantuml.jar"
+$graphviz = Join-Path $PSScriptRoot "graphviz"
+$graphvizdot = Join-Path $graphviz "dot.exe"
+
+java -jar "$plantuml" -graphvizdot "$graphvizdot" -output "$outputPath" -timeout 60 "$inputPath\*.wsd"
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Build failed! See log above for errors."
+}
